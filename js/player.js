70bda1b81ef6342d4c2f3d264b41ab55c051022d
Luan Luiz Gonçalves <luanlg.cco@gmail.com>
2020-08-04 13:44:59 -0300

    Add player

--- js/player.js
+++ js/player.js
@@ -1,7 +1,39 @@
 'use strict';
 
 class Player {
-  constructor(context) {
-    this.context = context;
+  constructor(appInstance) {
+    this.appInstance = appInstance;
+    this.audio = document.createElement('audio');
+    this.audio.controls = true;
+    this.audio.autoplay = true;
+    this.node = this.appInstance.context.createMediaElementSource(this.audio);
+    this.node.connect(this.appInstance.gain);
+    this.appInstance.setGain(1);
+    $("#player-controls").empty();
+    $("#player-controls").hide();
+    $("#player-file").show();
+    $("#player-file-display").removeClass("selected").html("Choose audio file");
+
+    let thisClass = this;
+    $(".custom-file-input").on("change", function() {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        thisClass.audio.src = this.result;
+
+      };
+      reader.readAsDataURL(this.files[0]);
+      $(this).siblings(".custom-file-label").addClass("selected").html($(this).val().split("\\").pop());
+      $("#player-controls").show();
+      $("#player-controls").html(thisClass.audio);
+    });
+  }
+
+  start() {
+    $("#player-controls").show();
+  }
+
+
+  stop() {
+    $("#player-controls").hide();
   }
 }
